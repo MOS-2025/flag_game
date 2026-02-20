f9e1e704f79ccdd7805160f27130b1c5a98d3112
MOS2025 <michealose@gmail.com>
2026-02-20 14:01:52 +0000

    Created DOM elements for Java Script moved location of bootstrap links in HTML file

---
+++ assets/script.js
@@ -0,0 +1,11 @@
+
+
+// DOM Elements
+const flagImg = document.getElementById("flagImg");
+const guessInput = document.getElementById("guessInput");
+const submitBtn = document.getElementById("submitBtn");
+const nextBtn = document.getElementById("nextBtn");
+const blankBtn = document.getElementById("blankBtn");
+const message = document.getElementById("message");
+const scoreDisplay = document.getElementById("score");
+const timerDisplay = document.getElementById("timer");
